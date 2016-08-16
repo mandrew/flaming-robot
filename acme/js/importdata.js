0e7d39dd3e844ff4f2c991b76a55b29adc89ab64
Mike Andrewartha <mandrewnz@gmail.com>
2016-08-16 15:57:12 +1200

    added files for acme company

---
+++ acme/js/importdata.js
@@ -0,0 +1,42 @@
+var data = [
+    {
+        "name": "Outlook basics",
+        "topic": "IT",
+        "description": "Learning how to use Microsoft Outlook to manage your email. This is a 2 day course focused on practical use of Outlook.",
+        "progress": 63,
+        "editable": true,
+        "deletable": false
+    },
+    {
+        "name": "Excel basics",
+        "topic": "IT",
+        "description": "Learning how to use Microsoft Excel to create spreadsheets. This is a 2 day course will give you enough information to get up to speed quickly and use Excel in your daily work.",
+        "progress": 0,
+        "editable": false,
+        "deletable": false
+    },
+    {
+        "name": "Outlook intermediate",
+        "topic": "IT",
+        "description": "This course expands on the knowledge you learnt in the Outlook basics course to provide you with a more in depth knowledge of Outlook. You'll learn how to filter your mail, perform advanced searches and set up mail rules.",
+        "progress": 100,
+        "editable": true,
+        "deletable": true
+    },
+    {
+        "name": "Organisational induction",
+        "topic": "General",
+        "description": "This course introduces you to working at ACME Ltd. All new employees must take this course.",
+        "progress": 100,
+        "editable": false,
+        "deletable": true
+    },
+    {
+        "name": "First Aid Basics",
+        "topic": "Health and Safety",
+        "description": "Learn basic first aid techniques.",
+        "progress": 92,
+        "editable": true,
+        "deletable": false
+    }
+]
---
+++ acme/js/main.js
@@ -0,0 +1,152 @@
+(function() {
+    var holder = $('.js-list-items')
+        elements = $();
+
+    if(data.length) {
+        // removes spaces out of title - used with images
+        function createUrl(title) {
+            return title.replace(/ /g,"-").toLowerCase();
+        }
+
+        // if data has items then remove no-items message
+        $('.no-items').remove();
+
+        $.each( data, function(i,e){
+            var listItem = $('<li>', {'class': 'course-item'})
+                courseContent = $('<div>', {'class': 'course-content'})
+                courseName = e["name"]
+                courseTopic = e["topic"]
+                courseDescription = e["description"]
+                courseProgress = e["progress"]
+                courseEdit = e["editable"]
+                courseDelete = e["deletable"]
+                courseUrl = createUrl(courseName);
+
+            if(courseName) {
+                // this relies on having a file that exists in /img with the correct name
+                listItem.append(
+                    $('<div>', {
+                        'class': 'course-image',
+                        'html': '<img src="img/' + courseUrl + '.jpg">'
+                    })
+                )
+            }
+            listItem.append(
+                $('<p>', {
+                    'class': 'course-topic',
+                    'html': courseTopic
+                })
+            )
+            listItem.append(courseContent)
+            courseContent.append(
+                $('<h3>', {
+                    'class': 'course-name',
+                    'html': courseName
+                }),
+                $('<p>', {
+                    'class': 'course-description',
+                    'html': courseDescription
+                })
+            )
+            if(courseProgress == 100) {
+                listItem.append(
+                    $('<div>', {
+                        'class': 'course-progress-meter',
+                        'html': '<div class="meter"><span style="width:'+ courseProgress +'%"><span class="course-progress-percent">' + courseProgress + '%</span></span></div><p class="course-progress-message">Congratulations you have finished this course</p>'
+                    })
+                )
+            } else {
+                if(courseProgress == 0) {
+                    courseContent.append(
+                        $('<p>', {
+                            'class': 'course-progress-button',
+                            'html': '<a class="btn">Start learning now!</a>'
+                        })
+                    )
+                } else {
+                    listItem.append(
+                        $('<div>', {
+                            'class': 'course-progress-meter',
+                            'html': '<div class="js-meter-animate meter"><span style="width:'+ courseProgress +'%"><span class="course-progress-percent">' + courseProgress + '%</span></span></div><p class="course-progress-message visuallyhidden">Course progress is: ' + courseProgress + '%</p>'
+                        })
+                    )
+                }
+            }
+            if(courseEdit || courseDelete) {
+                var adminBlock = $('<div>', {
+                    'class': 'course-admin hide',
+                    'html': "<p>Admin options</p>"
+                })
+                listItem.append(adminBlock)
+
+                if(courseEdit) {
+                    adminBlock.append(
+                        $('<button/>', {
+                            'class': 'course-edit btn',
+                            'value': "Edit",
+                            'html': "Edit"
+                        })
+                    )
+                }
+                if(courseDelete) {
+                    adminBlock.append(
+                        $('<button/>', {
+                            'class': 'course-delete btn red',
+                            'value': 'Delete',
+                            'html': "Delete"
+                        })
+                    )
+                }
+            }
+            listItem.append(
+                $('<a>', {
+                    'class': 'course-link',
+                    'url': '#'
+                })
+            )
+
+            elements = elements.add(listItem)
+        });
+        holder.append(elements);
+
+        // animate percent bar
+        $(".js-meter-animate > span").each(function() {
+            $(this).data("origWidth", $(this).width())
+                .width(0)
+                .animate({
+                    width: $(this).data("origWidth")
+                }, 1200);
+        });
+
+        // show/hide admin options
+        $(".js-admin").click(function() {
+            $(this).toggleClass('on');
+            $(".course-admin").each(function() {
+                $(this).toggleClass('hide');
+            })
+        });
+
+        // show/hide content
+        $('li.course-item').hover(
+            function(){
+                $(this).addClass('hover');
+            }, function(){
+                $(this).removeClass('hover');
+            }
+        );
+
+        // swap list/block items
+        $('.js-block-courses').click(function() {
+            $('.js-list-courses').removeClass('current');
+            $(this).addClass('current');
+            $('.js-list-items').removeClass('list')
+                .addClass('block');
+        });
+        $('.js-list-courses').click(function() {
+            $('.js-block-courses').removeClass('current');
+            $(this).addClass('current');
+            $('.js-list-items').removeClass('block')
+                .addClass('list');
+        });
+    }
+}());
