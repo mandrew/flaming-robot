59e5c9c8ff3441ee7103b76661076f2772b0ae84
Mike Andrewartha <mandrewnz@gmail.com>
2016-09-27 15:04:31 +1300

    added more styling and responsive fixes + second level template

--- marist/js/main.js
+++ marist/js/main.js
@@ -1,28 +1,49 @@
-/* carousel */
-var $carousel = $('.carousel').flickity({
-    prevNextButtons: false,
-    pageDots: false,
-    percentPosition: true,
-    wrapAround: false,
-    freeScroll: true,
-    contain: true
-});
-// Flickity instance
-var flkty = $carousel.data('flickity');
-// elements
-var $cellButtonGroup = $('.button-group--cells');
-var $cellButtons = $cellButtonGroup.find('.button');
+/* Menu - uses hoverIntent to control dropdown */
+(function() {
+    function showMenu(){
+		$(this).find(".menu2").addClass("hover");
+	}
+	function hideMenu(){
+		$(this).find(".menu2").removeClass("hover");
+	}
+    var config = {
+    	sensitivity: 5,
+    	interval: 100,
+    	over: showMenu,		// function = onMouseOver callback (REQUIRED)
+    	timeout: 400,		// number = milliseconds delay before onMouseOut
+    	out: hideMenu		// function = onMouseOut callback (REQUIRED)
+    };
+    $(".js-navbar ul li").hoverIntent( config );
+})();
 
-// update selected cellButtons
-$carousel.on( 'select.flickity', function() {
-    $cellButtons.filter('.is-selected')
-        .removeClass('is-selected');
-    $cellButtons.eq( flkty.selectedIndex )
-        .addClass('is-selected');
-});
+(function() {
+    /* HomePage: carousel */
+    var $carousel = $('.carousel').flickity({
+        prevNextButtons: false,
+        pageDots: false,
+        percentPosition: true,
+        wrapAround: false,
+        freeScroll: true,
+        contain: true
+    });
+    // Flickity instance
+    var flkty = $carousel.data('flickity');
+    // elements
+    var $cellButtonGroup = $('.button-group--cells');
+    var $cellButtons = $cellButtonGroup.find('.button');
 
-// select cell on button click
-$cellButtonGroup.on( 'click', '.button', function() {
-  var index = $(this).index();
-  $carousel.flickity( 'select', index );
-});
+    // update selected cellButtons
+    $carousel.on( 'select.flickity', function() {
+        $cellButtons.filter('.is-selected')
+            .removeClass('is-selected');
+        $cellButtons.eq( flkty.selectedIndex )
+            .addClass('is-selected');
+    });
+
+    // select cell on button click
+    $cellButtonGroup.on( 'click', '.button', function() {
+      var index = $(this).index();
+      $carousel.flickity( 'select', index );
+    });
+
+})();
